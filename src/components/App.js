bc912af4b9f98402f7629a1635d30b1bc91a56a8
rinku sutradhar <sopnonill87@gmail.com>
2022-06-27 17:23:30 +0600

    component structuring

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,13 @@
+import Home from "../pages/Home";
+import "../styles/App.css";
+import Layout from "./Layout";
+
 function App() {
-  return <div className="App">Hello WOrld</div>;
+  return (
+    <Layout>
+      <Home />
+    </Layout>
+  );
 }
 
 export default App;
